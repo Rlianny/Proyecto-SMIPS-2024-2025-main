e54087b4475c264a81ca8ed7f32a5115d57c3d6e
Lianny Revé Valdivieso <revelianny10@gmail.com>
2025-01-27 19:58:17 -0500

    Test Commit

--- assembler.py
+++ assembler.py
@@ -10,14 +10,17 @@ instructions = []
 relocations = []
 
 
+
 class AssemblerError(Exception):
     pass
+
 
 
 class AssemblerSyntaxError(AssemblerError):
     def __init__(self, line, reason):
         self.line = line
         self.reason = reason
+        
 
     def __str__(self):
         return "Syntax error on line %d: %s" % (self.line, self.reason)
